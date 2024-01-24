7aa078e325d9ef85fc27bb610a682808a808e1e4
Nanashi <johnnyjel1234567890@gmail.com>
2024-01-23 21:29:48 -0600

    Update zorin.sh

--- zorin.sh
+++ zorin.sh
@@ -56,14 +56,14 @@ if [ "$sixteen" = "true" ]; then
             # Copy zorin16.list mod
             sudo cp -f ./zorin16.list /etc/apt/sources.list.d/zorin.list
             # Add the required apt-key to be safe
-            curl -sS https://packages.zorinos.com/zorin_os_key.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/zorin.gpg
+            # curl -sS https://packages.zorinos.com/zorin_os_key.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/zorin.gpg
             # sudo gpg --keyserver key server.ubuntu.com --recv-key  5FD7496A07D323BC
             # sudo gpg -a --export 5FD7496A07D323BC | sudo apt-key add -
 elif [ "$sixteen" = "false" ]; then
             # Copy zorin17.list mod
             sudo cp -f ./zorin17.list /etc/apt/sources.list.d/zorin.list
             # Add the required apt-key to be safe
-            curl -sS https://packages.zorinos.com/zorin_os_key.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/zorin.gpg
+            # curl -sS https://packages.zorinos.com/zorin_os_key.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/zorin.gpg
             # sudo gpg --keyserver key server.ubuntu.com --recv-key  5FD7496A07D323BC
             # sudo gpg -a --export 5FD7496A07D323BC | sudo apt-key add -
 else
@@ -89,7 +89,7 @@ echo "Adding premium content..."
 echo ""
 
 # update packages
-sudo aptitude update
+sudo aptitude update ; sudo apt-get update
 
 if [ "$sixteen" = "true" ]; then   
             # install 16 pro content
